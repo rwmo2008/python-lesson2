39134826c457200547b14f0cf3faa64c15170d37
rwmo2008 <rwmo2008@gmail.com>
2023-10-06 15:57:41 -0400

    python lesson 2

---
+++ lesson2.py
@@ -0,0 +1,33 @@
+#variables
+shoe_size = 10
+print (shoe_size)
+
+shoe_size = 10.5
+print(shoe_size)
+
+quote = 'The doormat says "Welcome".'
+print(quote)
+
+#if using only of that quote type
+quote1 = "The doormat says \"Welcome\"."
+print(quote1)
+
+#tab, new line
+text = "1\t2\n3"
+print(text)
+
+#assign multiple variables in single line
+a, b = 1, 2
+print (a)
+print(b)
+
+#user input
+#value = input("Your name: ")
+#print(value)
+
+#user input of data type
+age = int(input("Your age: "))
+future_age = age + 1
+print (future_age)
+
+
---
+++ lesson2_math_ops.py
@@ -0,0 +1,22 @@
+#concatenation
+print("New " + "York")
+
+print("=" * 80)
+#repeat string
+print ("and over " * 3)
+
+#string length
+word = "What is my string length?"
+print (len(word))
+
+#indexing
+phrase = "wombo combo"
+print (phrase [0])
+
+#change character in string
+phrase = 'W' + phrase[1:]
+print(phrase)
+
+#slicing
+print (phrase [1:2])
+print (phrase [6:])
